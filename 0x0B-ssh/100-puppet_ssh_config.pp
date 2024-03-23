37b506c680f7a1ad232dccad92b31397c5255d17
coleYab <yabume123@gamil.com>
2024-03-23 16:26:32 +0300

    Addedd fuke

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,11 @@
+# file to create file lines
 
+file_line { 'SetPassAuth':
+    path: '/etc/ssh/ssh_config',
+    line: 'PasswordAuthentucation no',
+}
+
+file_line { 'SetIdentityFile':
+    path: '/etc/ssh/ssh_config',
+    line: 'IdentityFile ~/.ssh/school',
+}
